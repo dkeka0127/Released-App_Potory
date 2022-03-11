512febb6a38a6e79559b47a78ae836557797b0c8
sori127 <39721950+dkeka0127@users.noreply.github.com>
2022-03-11 11:43:43 +0900

    오류 수정

--- src/screens/info/components/SettingContent.tsx
+++ src/screens/info/components/SettingContent.tsx
@@ -21,40 +21,6 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 // Variable
 const IconSize = 17;
 const IconColor = '#111';
-const [result, setResult] = useState<string>(''); // Kakao Login
-// 로그아웃
-const signOutWithKakao = async (): Promise<void> => {
-  const message = await logout();
-  setResult(message);
-};
-// 회원탈퇴
-const unlinkKakao = async (): Promise<void> => {
-  const message = await unlink();
-  setResult(message);
-};
-
-// Alert Message
-const AlertText = title => {
-  Alert.alert(
-    '',
-    title === '로그아웃'
-      ? '로그아웃 하시겠습니까 ?'
-      : '회원탈퇴 시 회원님의 소중한 추억이 \n 삭제되며 복구가 불가합니다. \n 정말로 탈퇴하시겠습니까 ?',
-    [
-      {
-        text: '취소',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: '확인',
-        onPress: () => {
-          title === '로그아웃' ? signOutWithKakao() : unlinkKakao();
-        },
-      },
-    ],
-  );
-};
 
 // [컴포넌트] Logout / Withdrawal
 const SettingAccount = ({iconName, title}) => {
@@ -107,6 +73,42 @@ const SettingContectTitle = ({title}) => {
 };
 
 function SettingContent() {
+  // Kakao Login
+  const [result, setResult] = useState<string>(''); // Kakao Login
+  // 로그아웃
+  const signOutWithKakao = async (): Promise<void> => {
+    const message = await logout();
+    setResult(message);
+  };
+  // 회원탈퇴
+  const unlinkKakao = async (): Promise<void> => {
+    const message = await unlink();
+    setResult(message);
+  };
+
+  // Alert Message
+  const AlertText = title => {
+    Alert.alert(
+      '',
+      title === '로그아웃'
+        ? '로그아웃 하시겠습니까 ?'
+        : '회원탈퇴 시 회원님의 소중한 추억이 \n 삭제되며 복구가 불가합니다. \n 정말로 탈퇴하시겠습니까 ?',
+      [
+        {
+          text: '취소',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: '확인',
+          onPress: () => {
+            title === '로그아웃' ? signOutWithKakao() : unlinkKakao();
+          },
+        },
+      ],
+    );
+  };
+
   return (
     <ScrollView style={styles.content}>
       {/* 게시판 */}
